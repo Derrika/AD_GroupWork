c3d175a86ae67869a2b06c49c4aad2cefedfec49
Derrika Coley <45216120+Derrika@users.noreply.github.com>
2021-06-14 19:58:14 -0500

    Update SQL Code Challenges.sql

--- SQL Code Challenges.sql
+++ SQL Code Challenges.sql
@@ -217,6 +217,28 @@ c.	UPDATE Customers SET FavoriteDish="9" WHERE CustomerID="42"; OR
 	ON Orders.CustomerID=Customers.CustomersID
 	GROUP BY Orders.CustomersID 
 	ORDER BY OrderCount DESC LIMIT 5;
+	
+	
+
+
+
+
+/***************************************************************************   QUESTION *********************************************
+	Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table. 
+	The STATION table is described as follows:
+
+	where LAT_N is the northern latitude and LONG_W is the western longitude.
+	For example, if there are three records in the table with CITY values 'New York',
+	'New York', 'Bengalaru', there are 2 different city names: 'New York' and 'Bengalaru'. 
+	The query returns , because 
+
+**************************************************************************************************************************************/
+
+
+1. -- *****************************************  ANSWER ***************************
+	
+	SELECT (COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+	
 
 
 
